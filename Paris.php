bf14ae7bf7ee5e3eb945d4e02d3af83d528ade1c
njkf <43401909+njkf@users.noreply.github.com>
2019-05-06 12:24:17 +0200

    Update Paris.php

--- Paris.php
+++ Paris.php
@@ -1,7 +1,17 @@
+<?php
 
+if($heure > 7 || $heure < 19)
+{
+  echo '<style> body {
+  background-color : #000;
+  color : #FFF;}
+  </style>';
+}
 
 
 for ($i = 0; $i < 5; $i++)
 {
   echoc '<img src= "https://www.prevision-meteo.ch/uploads/widget/paris_' .$i '.png">'
 }
+
+?>
